d801f74c1de48ff82a764ea5641d18e4b1fef264
sankalp0104 <sankalp0104@hotmail.com>
2021-12-25 00:54:52 +0530

    Same lines to functions

--- card_trick.py
+++ card_trick.py
@@ -1,6 +1,23 @@
 import time
 import random
 
+
+def update_rows(rows, entered_row):
+    new_list = []
+    new_list.extend(rows[(entered_row + 1) % 3])
+    new_list.extend(rows[entered_row])
+    new_list.extend(rows[(entered_row + 2) % 3])
+
+    new_rows = []
+    new_rows.append(new_list[::3])
+    new_rows.append(new_list[1::3])
+    new_rows.append(new_list[2::3])
+
+    for i in range(3):
+        print(f"Row {i+1}:", *new_rows[i], sep='\t')
+    return new_rows, new_list
+
+
 suits = ['♦', '♥', '♠', '♣']
 card_numbers = [str(i) for i in range(2, 11)]
 card_numbers.extend(['K', 'Q', 'J', 'A'])
@@ -23,49 +40,15 @@ while 1:
     for i in range(3):
         print(f"Row {i+1}:", *rows[i], sep='\t')
 
-    entered_row = int(
-        input("\nChoose a card and enter its row number [1,2,3]: "))
-    print()
-    entered_row -= 1
-
-    new_rows = []
-    new_rows.extend(rows[(entered_row + 1) % 3])
-    new_rows.extend(rows[entered_row])
-    new_rows.extend(rows[(entered_row + 2) % 3])
-
-    rows[0] = new_rows[::3]
-    rows[1] = new_rows[1::3]
-    rows[2] = new_rows[2::3]
+    m1 = "\nChoose a card and enter its row number [1,2,3]: "
+    m2 = "\nEnter the row number of the chosen card [1,2,3]: "
 
     for i in range(3):
-        print(f"Row {i+1}:", *rows[i], sep='\t')
-
-    entered_row = int(
-        input("\nEnter the row number of the chosen card [1,2,3]: "))
-    print()
-    entered_row -= 1
-
-    new_rows = []
-    new_rows.extend(rows[(entered_row + 1) % 3])
-    new_rows.extend(rows[entered_row])
-    new_rows.extend(rows[(entered_row + 2) % 3])
-
-    rows[0] = new_rows[::3]
-    rows[1] = new_rows[1::3]
-    rows[2] = new_rows[2::3]
-
-    for i in range(3):
-        print(f"Row {i+1}:", *rows[i], sep='\t')
-
-    entered_row = int(
-        input("\nEnter the row number of the chosen card [1,2,3]: "))
-    print()
-    entered_row -= 1
-
-    new_rows = []
-    new_rows.extend(rows[(entered_row + 1) % 3])
-    new_rows.extend(rows[entered_row])
-    new_rows.extend(rows[(entered_row + 2) % 3])
+        message = m1 if i == 0 else m2
+        entered_row = int(input(message))
+        entered_row -= 1
+        print()
+        rows, new_list = update_rows(rows, entered_row)
 
     print("\nYour cards is ...")
     time.sleep(2)
@@ -73,7 +56,7 @@ while 1:
     time.sleep(2)
     print("Wait for it ...")
     time.sleep(2)
-    print("»  » ", new_rows[10], " «  «")
+    print("»  » ", new_list[10], " «  «")
 
     play_again = input("Do you want to play again? [y/n]: ")
     if play_again not in ['y', 'Y']: break
